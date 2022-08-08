2cafa8621548a08f97507bb225060155a3c5444a
Rounak Banik <rounakbanik@gmail.com>
2022-08-08 21:04:59 +0530

    Change contract address

--- src/contracts/contractData.js
+++ src/contracts/contractData.js
@@ -1,7 +1,7 @@
 import lotbContract from './LegendsOnTheBlock.json';
 
 //Testnet address: "0x0147Ecd070f312eeE7b851b82385F9b2DF979e31";
-const contractAddress = "0x9B750464ddc4dCeb5A2b1e0D5e3e1F2DA1E61Ce7"
+const contractAddress = "0x6EA9818a25a4D50c320dcef4555c9b5AC1a7954B"
 const contractAbi = lotbContract.abi;
 
 export { contractAddress, contractAbi }
